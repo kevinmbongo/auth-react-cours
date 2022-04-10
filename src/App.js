1e09ca8655846a0c95a298a4cc7fb03d64801361
kevinmbongo <mbongo.kevin.pro@gmail.com>
2022-04-10 16:33:05 +0200

    feat(Navbar): implement Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="display-1">Bootstrap !</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container p-5">
+      <h1 className="display-2 text-light">Hi, sign up or Sign in</h1>
+    </div>
+  );
+}
